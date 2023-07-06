e488a79662b3f7ab77f806bf32094cf4c53e974f
taktak-0319 <tka150319@icloud.com>
2023-07-07 00:42:02 +0900

    修正

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainPage from "./components/Pages/MainPage";
 import "./style.css";
-import { AlertHandlerProvider } from "./contexts/alert_hundler";
+import { AlertHandlerProvider } from "./contexts/alert_handler";
 import Alertmanager from "./components/Organisms/AlertManager";
 
 ReactDOM.render(
